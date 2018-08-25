49190d7024cf4f37674798cc3c9cf220836ecf2d
NIC-PC <NIC-PC@NIC-PC-HP>
2018-08-25 12:39:01 +0530

    First Commit Project

---
+++ src/main/java/com/example/MyPractice/MyPracticeApplication.java
@@ -0,0 +1,12 @@
+package com.example.MyPractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyPracticeApplication.class, args);
+	}
+}
